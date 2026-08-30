2bb702e945ed9ef221bdd7d61e052176d181e4e8
Fred <36470355+ajp-lab@users.noreply.github.com>
2026-08-30 22:21:58 +0200

    Bugfix in 03-test-scan-dcp585cw.sh, no explicit devicename

--- br-print/03-test-scan-dcp585cw.sh
+++ br-print/03-test-scan-dcp585cw.sh
@@ -6,7 +6,7 @@ echo "=== 1. Verifying scanner detection ==="
 docker exec -it $CONTAINER scanimage -L
 
 echo "=== 2. Executing test scan ==="
-docker exec -it $CONTAINER scanimage -d "brother3:DCP-585CW" --format=png --resolution 150 -o /tmp/testscan.png
+docker exec -it $CONTAINER scanimage --format=png --resolution 150 -o /tmp/testscan.png
 
 echo "=== 3. Copying scanned file to Downloads ==="
 docker cp $CONTAINER:/tmp/testscan.png "$OUTPUT_FILE"
